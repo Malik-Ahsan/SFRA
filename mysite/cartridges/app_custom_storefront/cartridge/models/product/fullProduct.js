0ad53c8f077f55c939fff101be218bafda88ae2d
Malik-Ahsan <ahsanakbar1010@gmail.com>
2023-05-09 14:29:22 +0500

    final code

--- mysite/cartridges/app_custom_storefront/cartridge/models/product/fullProduct.js
+++ mysite/cartridges/app_custom_storefront/cartridge/models/product/fullProduct.js
@@ -5,5 +5,9 @@ var base = module.superModule;
 module.exports = function fullProduct(product, apiProduct, options){
     base.call(this,product, apiProduct, options);
 
-    
+    Object.defineProperty(product, 'msg',{
+        enumerable: true,
+        value: 'New '
+    });
+    return product;
 }
