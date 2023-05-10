314afae243c887fccf053ca389a105dec90b3112
Malik-Ahsan <ahsanakbar1010@gmail.com>
2023-05-10 18:17:58 +0500

    Q11 done

--- mysite/cartridges/app_custom_storefront/cartridge/controllers/Training.js
+++ mysite/cartridges/app_custom_storefront/cartridge/controllers/Training.js
@@ -5,13 +5,23 @@ var server = require('server');
 server.get('Product',function(req,res,next){
     var productMgr = require('dw/catalog/ProductMgr');
 
-    var product = productMgr.getProduct('25517958M');
-    res.json({
-        productId: product.ID,
-	    productName: product.name,
-	    productDescription: product.shortDescription.toString()
-    });
+    var product = productMgr.getProduct(req.querystring.pid);
+    if(product){
+        res.json({
+            productId: product.ID,
+            productName: product.name,
+            productDescription: product.shortDescription.toString()
+        });
+    }else{
+        res.json({
+            error: 'Product id is Invalid'
+        });
+    }
     next();
 });
 
 module.exports = server.exports();
+
+//querystring = req.querystring.pid
+//var params = querystring;
+//var productId = params.pid
